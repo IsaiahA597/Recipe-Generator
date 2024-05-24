ea7995c6f77b8bf0a56f19faaa491c89d91747ba
IsaiahA597 <iarreol1@ucsc.edu>
2024-05-23 18:24:25 -0700

    .

--- js/page.js
+++ js/page.js
@@ -1,3 +1,7 @@
 document.getElementById("Recipes").addEventListener("click", function() {
     document.getElementById("output").style.display = "block";
   });
+
+  document.getElementById("About").addEventListener("click", function() {
+    document.getElementById("output1").style.display = "block";
+  });
