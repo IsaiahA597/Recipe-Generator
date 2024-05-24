f35cdd558d45da710d454376235f252441ec5311
IsaiahA597 <iarreol1@ucsc.edu>
2024-05-24 15:54:05 -0700

    .

--- js/page.js
+++ js/page.js
@@ -5,3 +5,20 @@ document.getElementById("Recipes").addEventListener("click", function() {
   document.getElementById("About").addEventListener("click", function() {
     document.getElementById("output1").style.display = "block";
   });
+
+  document.getElementById('Recipe').addEventListener('click', function() {
+    const paper = document.querySelector('.paper');
+    
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    
+    paper.style.transition = 'none';
+    paper.style.bottom ='-100%';
+    paper.offsetHeight;
+    paper.style.transition = '';
+    
+    paper.style.backgroundColor = randomColor;
+    paper.style.bottom = '10px';
+    
+    });
+    
+  
